b09c94e77fa668ed0a23b56159349bde82ae53d5
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-04 09:31:51 -0700

    Test added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # testservice_03
+
+echo "# testservice_03" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/moumitacloud08/testservice_03.git
+git push -u origin main
+
+
+---------------------
+@DataJpaTest
---
+++ src/test/java/com/example/testservice_03/entity/EmployeeTest.java
@@ -0,0 +1,29 @@
+package com.example.testservice_03.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DataJpaTest
+public class EmployeeTest {
+
+    private static final Employee EMPLOYEE_1 = new Employee(1, "John" ,"Doe" ,"John.Doe@corp.com");
+    private static final Employee EMPLOYEE_2 = new Employee(2,"Dave", "Doyle" ,"Dave.Doyle@corp.com");
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void shouldStoreEmployeeAndFindById(){
+        testEntityManager.merge(EMPLOYEE_1);
+        testEntityManager.merge(EMPLOYEE_2);
+
+        assertEquals(EMPLOYEE_1,testEntityManager.find(Employee.class, 1));
+        assertEquals(EMPLOYEE_2, testEntityManager.find(Employee.class,2));
+    }
+}
